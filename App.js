5db51a9c509a73687850083207f7b65b995d165f
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-07-27 20:15:52 +0200

    commented out unused lines

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Start from './components/Start';
-// import Screen2 from './components/Screen2';
 import Chat from './components/Chat';
 import 'react-native-gesture-handler'; // import react native gesture handler
 import { NavigationContainer } from '@react-navigation/native'; // import react Navigation
 import { createStackNavigator } from '@react-navigation/stack';
+// import CustomActions from './components/CustomActions';
 
 // Create the navigator
 const Stack = createStackNavigator();
 
 export default class App extends React.Component {
+
+  //create the circle button
+  // renderCustomActions = (props) => {
+  //   return <CustomActions {...props} />;
+  // };
+
   render() {
     return (
       <NavigationContainer>
